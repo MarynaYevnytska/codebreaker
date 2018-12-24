eb3a8f564525eabec4bc6df22b6fb739bd0dd421
Yevnytska Maryna <me_home@ukr.net>
2018-12-24 15:17:23 +0200

    Console RSpec with mistake +

--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Console do
-let (:console) {described_class.new()}
+let (:console) {described_class.new(nil)}
 context "when gem run user see greeting message" do
   specify { expect { Console.new }.to output(print I18n.t('greeting')).to_stdout }
 end
@@ -11,23 +11,23 @@ context "when an user input is valid _POSITIVE" do
     end
     it "if user want to view rules  and press `rules`"do
     allow(console).to receive(:choice).with(console.question{}).and_return(Console::MENU[:game_rules])
-    expect(console).to receive(:choose_the_command).once
+    expect(console).to receive(:rules).once
     console.rules
     end
     it "if user want to view statistics and press `stats`"do
     allow(console).to receive(:choice).with(console.question{}).and_return(Console::MENU[:stats])
-    expect(console).to receive(:choose_the_command).once
+    expect(console).to receive(:stats).once
     console.stats
     end
     it "if user want to start and press `start`" do
     allow(console).to receive(:choice).with(console.question{}).and_return(Console::MENU[:game_start])
-    expect(console).to receive(:choose_the_command).once
+    expect(console).to receive(:start).once
     console.start
     end
 
     it "if user want to exit and press `exit`" do
     allow(console).to receive(:choice).with(console.question{}).and_return(Console::MENU[:exit])
-    expect(console).to receive(:choose_the_command).once
+    expect(console).to receive(:goodbye).once
     console.goodbye
     end
   end
